07b1d9de9469ddf073de1b2d2db05bfbc63dd691
Rob <coleman.rob@gmail.com>
2016-01-10 14:20:01 -0500

    added comments

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,33 +1,33 @@
 
 # coding: utf-8
 
-# In[9]:
+# importing packages
 
 import os
 from PyPDF2 import PdfFileReader, PdfFileMerger
 
 
-# In[38]:
+# identify two seperate locations of files
 
 maps_dir = '/home/rob/projects/pdfscript/maps'
 fsheets_dir = '/home/rob/projects/pdfscript/fsheets'
 
 
-# In[39]:
+# convert dir to lists and define merger
 
 map_files = [f for f in os.listdir(maps_dir)]
 fsheet_files = [f for f in os.listdir(fsheets_dir)]
 merger = PdfFileMerger()
 
 
-# In[40]:
+# loop through list and map and factsheet together
 
 for maps, fsheets in zip(map_files, fsheet_files):
     merger.append(PdfFileReader(os.path.join(maps_dir, maps), "rb"))
     merger.append(PdfFileReader(os.path.join(fsheets_dir, fsheets), "rb"))
 
 
-# In[42]:
+# write output into one document
 
 merger.write(os.path.join(maps_dir, "merged_full.pdf"))
 
