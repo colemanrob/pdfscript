df6ec1fcbaa4d76da0cecc57d273f6e1d883fbfa
Rob <coleman.rob@gmail.com>
2016-01-11 18:59:22 -0500

    added function to sort

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,33 +1,44 @@
 
 # coding: utf-8
 
-# importing packages
+# In[30]:
 
 import os
 from PyPDF2 import PdfFileReader, PdfFileMerger
+import re
 
 
-# identify two seperate locations of files
+# In[31]:
 
-maps_dir = '/home/rob/projects/pdfscript/maps'
-fsheets_dir = '/home/rob/projects/pdfscript/fsheets'
+maps_dir = 'C:\\Users\\ColemaRo\\Desktop\\2016 OEAS\\v5\\maps'
+fsheets_dir = 'C:\\Users\\ColemaRo\\Desktop\\2016 OEAS\\v5\\fsheets'
 
 
-# convert dir to lists and define merger
+# In[32]:
+
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+
+# In[39]:
 
 map_files = [f for f in os.listdir(maps_dir)]
 fsheet_files = [f for f in os.listdir(fsheets_dir)]
+map_files.sort()
+fsheet_files = natural_sort(fsheet_files)
 merger = PdfFileMerger()
 
 
-# loop through list and map and factsheet together
+# In[40]:
 
-for maps, fsheets in zip(map_files, fsheet_files):
-    merger.append(PdfFileReader(os.path.join(maps_dir, maps), "rb"))
+for fsheets, maps in zip(fsheet_files, map_files):
     merger.append(PdfFileReader(os.path.join(fsheets_dir, fsheets), "rb"))
+    merger.append(PdfFileReader(os.path.join(maps_dir, maps), "rb"))
 
 
-# write output into one document
+# In[41]:
 
 merger.write(os.path.join(maps_dir, "merged_full.pdf"))
 
